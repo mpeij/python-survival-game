05bbee9a5806b63a796da1ee220833837d10a4f8
sterhart <42876663+sterhart@users.noreply.github.com>
2018-09-22 13:52:58 +0200

    Update BiomeGen.py

--- BiomeGen.py
+++ BiomeGen.py
@@ -1,2 +1,18 @@
-class biome()
-    
+import random
+
+newTerrain = True
+water = False
+TerrainWater = ["Creek","River","Lake","Stream","Waterfall"]
+TerrainGround = ["Dirt","Grass","Rocks","Podzol"]
+TerrainAttribute = ["Trees","Cliffs"]
+
+
+if newTerrain == True:
+	water = random.choice([True, False])
+	TGF = random.choice(TerrainGround)	
+	TAF = random.choice(TerrainAttribute)
+	if water == True:
+		TWF = random.choice(TerrainWater)
+		print (TGF,TAF,TWF)
+	else:
+		print (TGF,TAF)
