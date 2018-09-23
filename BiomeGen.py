78a6650825f9c897a5ab5ef826ec6c9b4ef78b98
sterhart <42876663+sterhart@users.noreply.github.com>
2018-09-23 12:42:45 +0200

    Update BiomeGen.py

--- BiomeGen.py
+++ BiomeGen.py
@@ -2,17 +2,18 @@ import random
 
 newTerrain = True
 water = False
-TerrainWater = ["Creek","River","Lake","Stream","Waterfall"]
-TerrainGround = ["Dirt","Grass","Rocks","Podzol"]
-TerrainAttribute = ["Trees","Cliffs"]
-
+TerrainWater = ["creek","river","lake","stream","waterfall"]
+TerrainGround = ["grassy","rocky","sandy"]
+TerrainAttribute = ["trees","cliffs"]
+TerrainLevel = ["mountains","lowlands","highlands"]
 
 if newTerrain == True:
 	water = random.choice([True, False])
 	TGF = random.choice(TerrainGround)	
 	TAF = random.choice(TerrainAttribute)
+	TLF = random.choice(TerrainLevel)
 	if water == True:
 		TWF = random.choice(TerrainWater)
-		print (TGF,TAF,TWF)
+		print ("There are", TGF, TLF, "with", TAF, "and a", TWF)
 	else:
-		print (TGF,TAF)
+		print ("There are", TGF, TLF, "with", TAF)
